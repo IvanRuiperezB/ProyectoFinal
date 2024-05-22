9cfc72dabc15e75db1d728562ce01867620ef017
Ivan Ruiperez <ivanruiperez.instituto@gmail.com>
2024-05-22 13:39:19 +0200

    pagina player/player

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, redirect, url_for
 import os
 import requests
 app = Flask(__name__)
@@ -8,18 +8,26 @@ def inicio():
     return render_template("inicio.html")
 
 @app.route('/players')
-def jugadores():
+def players():
     return render_template("players.html")
 
-def jugador():
+@app.route('/player',methods=["POST"])
+def player():
+    player = request.form.get("player")
+    return redirect(url_for('perfil', player=player))
+
+@app.route('/player/<player>')
+def perfil(player):
+    key=os.environ["key"]
     URL_jugador="https://api.brawlstars.com/v1/players/%23"
     headers={'Authorization': f'Bearer {key}'}
-    tag=request.form.get("player")
-    r=requests.get(URL_jugador+tag,headers=headers)
+    r=requests.get(URL_jugador+player,headers=headers)
     if r.status_code == 200:
-        jugador=r.json()        
-        return render_template("player.html")
+        jugador=r.json()
+        brawlers=len(jugador["brawlers"])
+        return render_template("player.html",jugador=jugador,brawlers=brawlers)
     else:
         jugador=0
         return render_template("player.html",jugador=jugador)
+
 app.run("0.0.0.0",5000,debug=True)
